17cfb04ec9550c0ac7e13f142d8f031f604f4934
Juan Rey <juan.rey@tuta.com>
2024-03-30 01:25:25 +0100

    crear y mostrar tareas

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
-
+from .forms import TaskForm
+from .models import Tasks
 
 # Create your views here.
 
@@ -66,9 +67,31 @@ def signin(request):
             return redirect('tasks')
 
 def tasks(request):
-    return render(request, "tasks.html")
+    tasks = Tasks.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, "tasks.html", {'tasks' : tasks})
 
 
+
+def create_task(request):
+
+    if request.method == "GET":
+        return render(request, 'create_task.html', {
+            'form' : TaskForm
+        })
+    else:
+        try: 
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except:
+            return render(request, 'create_task.html',{
+                'form' : TaskForm,
+                'error' : 'Introduce datos válidos'
+            })
+    
+        
 
 
 
